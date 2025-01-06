b3aa2b245aa878dcb92cc8dedbd65dfa28bc982a
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 02:29:54 -0600

    Added to file: App.jsx - Added unordered list & paragraph tags (adding text next)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Section title="What is react?">
+        <p></p>
+      </Section>
+
+      <Section title="Benefits of react">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </Section>
     </div>
   );
 }
