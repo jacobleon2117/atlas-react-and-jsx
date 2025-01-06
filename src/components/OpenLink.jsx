695ff86b83409e9bb75d19f46ee16ab3d942b0d5
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 02:56:39 -0600

    Added to file: OpenLink.jsx - Created component with props to handle opening links in new tab

--- src/components/OpenLink.jsx
+++ src/components/OpenLink.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+import Open from "../assets/open.svg";
+
+const OpenLink = ({ link }) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <img src={Open} alt="Open Link" />
+    </a>
+  );
+};
+
+export default OpenLink;
