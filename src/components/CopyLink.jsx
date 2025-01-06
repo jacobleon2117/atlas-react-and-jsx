887ba0aba8f2425b3c72c6ac6814c527619fed7b
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 03:04:20 -0600

    Added to file: CopyLink.jsx - Created component with props to handle copying links to clipboard

--- src/components/CopyLink.jsx
+++ src/components/CopyLink.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import Copy from "../assets/copy.svg";
+
+const CopyLink = ({ link }) => {
+  const handleClick = () => {
+    navigator.clipboard.writeText(link);
+  };
+
+  return (
+    <img src={Copy} alt="Copy Link" className="Copy" onClick={handleClick} />
+  );
+};
+
+export default CopyLink;
