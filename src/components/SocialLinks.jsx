f62bce07d4268c619f083527bc132e7b2e37fabb
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 02:07:49 -0600

    Added function, SocialLinks to SocialLinks.jsx. Github + Linkedin icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,1 +1,27 @@
 import React from "react";
+
+import LinkedIn from "../assets/linkedin.svg";
+import GitHub from "../assets/github.svg";
+
+const SocialLinks = () => {
+  return (
+    <div className="social-links">
+      <a
+        href="https://www.linkedin.com/in/jacobleon02"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={LinkedIn} alt="LinkedIn Profile" />
+      </a>
+      <a
+        href="https://github.com/jacobleon2117/atlas-react-and-jsx"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={GitHub} alt="Github Repository" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
