691f77c109a8dfa06bea3bc8f5b535d62167a9af
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 02:42:16 -0600

    Added to: App.jsx - Added text to <p> tags & to the unordered list.

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,23 @@ function App() {
       <Header />
 
       <Section title="What is react?">
-        <p></p>
+        <p>
+          React is a library for building web and native user interfaces. It
+          allows developers to create user interfaces from individual pieces
+          called components. These components are like building blocks that can
+          be created, customized, and combined to build entire screens, pages,
+          and applications. This component based approach makes it easy to
+          manage and reuse code across your project.
+        </p>
       </Section>
 
       <Section title="Benefits of react">
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Virtual DOM</li>
+          <li>Fast rendering</li>
+          <li>Component based architecture</li>
+          <li>Super efficent, easy to learn and fast!</li>
+          <li>Guarantee stable code</li>
         </ul>
       </Section>
     </div>
