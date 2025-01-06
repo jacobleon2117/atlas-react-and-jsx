b475a94e755a38d0283e0537753a34cd4780e3b0
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 02:13:16 -0600

    Added component Header.jsx to render Social + Greeting components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import Greeting from "./Greeting";
+import SocialLinks from "./SocialLinks";
+
+const Header = () => {
+  retun(
+    <nav>
+      <Greeting />
+      <SocialLinks />
+    </nav>
+  );
+};
+
+export default Header;
