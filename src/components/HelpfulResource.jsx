e1408bd0cbe1c2281a3f1daa2df466edcced12e1
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 03:05:02 -0600

    Added to file: HelpfulResource.jsx - Created component that combines label, OpenLink, and CopyLink components

--- src/components/HelpfulResource.jsx
+++ src/components/HelpfulResource.jsx
@@ -1,1 +1,15 @@
 import React from "react";
+import Open from "./OpenLink";
+import Copy from "./CopyLink";
+
+const HelpfulResource = ({ link, label }) => {
+  return (
+    <div className="helpful-resource">
+      {label}
+      <Open link={link} />
+      <Copy link={link} />
+    </div>
+  );
+};
+
+export default HelpfulResource;
