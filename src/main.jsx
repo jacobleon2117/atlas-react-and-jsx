d68e96ae2fa68468502bb8e661c923b9cc9d61df
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 01:30:18 -0600

    fixing main.jsx to render correctly

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-// Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
