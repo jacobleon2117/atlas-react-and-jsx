feceeb9b58e56c6e4e4cf3bb421f657b8cadd0e4
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-06 03:22:55 -0600

    Fixed line 13: classname="Copy - The class name ( copy ) had an uppercase C which caused the clickable visablibtly

--- src/components/CopyLink.jsx
+++ src/components/CopyLink.jsx
@@ -7,7 +7,13 @@ const CopyLink = ({ link }) => {
   };
 
   return (
-    <img src={Copy} alt="Copy Link" className="Copy" onClick={handleClick} />
+    <img
+      src={Copy}
+      alt="Copy Link"
+      // Fixed line 13: "classname="Copy" - The class name ( copy ) had an uppercase C which caused the clickable visablibtly
+      className="copy"
+      onClick={handleClick}
+    />
   );
 };
 
